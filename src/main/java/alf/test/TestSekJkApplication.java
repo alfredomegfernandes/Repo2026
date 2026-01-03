77ce92b80b8728fe61566327388eab728b605d30
alfredomegfernandes <alfredomegfernandes@gmail.com>
2026-01-03 16:45:05 +0000

    commit inicial

---
+++ src/main/java/alf/test/TestSekJkApplication.java
@@ -0,0 +1,13 @@
+package alf.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSekJkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSekJkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alf/test/TestSekJkApplicationTests.java
@@ -0,0 +1,13 @@
+package alf.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSekJkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
