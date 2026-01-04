d3125daec9f69fba650fd2012c5ee4c411884ee7
alfredomegfernandes <alfredomegfernandes@gmail.com>
2026-01-04 01:13:41 +0000

    teste jk run

--- src/main/java/alf/test/TestSekJkApplication.java
+++ src/main/java/alf/test/TestSekJkApplication.java
@@ -8,6 +8,9 @@ public class TestSekJkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestSekJkApplication.class, args);
+	
+		System.out.println("roll");
 	}
 
+	
 }
